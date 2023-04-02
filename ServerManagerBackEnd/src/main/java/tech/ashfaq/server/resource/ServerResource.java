46c11635bd798572b42740f0fe56fbfca9666369
AhamedAfq <ashfakahamed142@gmail.com>
2023-04-02 12:09:29 +0530

    Addition of the Postman Collection

--- ServerManagerBackEnd/src/main/java/tech/ashfaq/server/resource/ServerResource.java
+++ ServerManagerBackEnd/src/main/java/tech/ashfaq/server/resource/ServerResource.java
@@ -1,7 +1,6 @@
 package tech.ashfaq.server.resource;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.ashfaq.server.model.Response;
