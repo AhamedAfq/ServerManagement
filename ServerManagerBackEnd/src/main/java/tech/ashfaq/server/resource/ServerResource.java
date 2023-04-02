e7cf67aff6fe6c2025459b829454acc90831e92a
AhamedAfq <ashfakahamed142@gmail.com>
2023-04-02 15:49:10 +0530

    Adding the image folder inside the project directory

--- ServerManagerBackEnd/src/main/java/tech/ashfaq/server/resource/ServerResource.java
+++ ServerManagerBackEnd/src/main/java/tech/ashfaq/server/resource/ServerResource.java
@@ -118,6 +118,6 @@ public class ServerResource {
 
     @GetMapping(path = "/image/{fileName}", produces = IMAGE_PNG_VALUE)
     public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
-        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "/Downloads/image/" + fileName));
+        return Files.readAllBytes(Paths.get(System.getProperty("user.dir") + "/image/" + fileName));
     }
 }
